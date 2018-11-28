ea3236d821fceb79012cc77f319144edfb566c2e
Tristram Newman <tristram.newman@gmail.com>
2018-11-28 22:08:08 +0000

    Implemented SVM

--- assignment1.py
+++ assignment1.py
@@ -1,22 +1,13 @@
 import os
 
-from stemmer import PorterStemmer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 from sklearn.feature_extraction.text import CountVectorizer
+from scipy.sparse import vstack
 
-# Independent variable columns:
-# *****************************
-# Features: unigrams, bigrams, unigrams + bigrams
-# Stemming?
-# Frequency vs Presence?
+from stemmer import PorterStemmer
 
-# Dependent variable columns:
-# ***************************
-# Unsmoothed Naive Bayes
-# Smoothed Naive Bayes
-# SVM
-
-def naive_bayes(folds, smoothed, ngrams, stemming, binary):
+def classify(folds, nb_or_svm, ngrams, stemming, binary):
     p = PorterStemmer()
 
     vectorizer = CountVectorizer(input="filename", \
@@ -25,40 +16,33 @@ def naive_bayes(folds, smoothed, ngrams, stemming, binary):
                                  binary=binary, \
                                  min_df=4, max_df=1.0)
 
-    X = vectorizer.fit_transform([f for fold in folds for f in fold])
+    X = vectorizer.fit_transform([f[0] for fold in folds for f in fold])
 
     accuracies = []
     for i in range(len(folds)):
-        classifier = MultinomialNB(alpha=(1.0 if smoothed else 1.0e-10))
-
-        for j in range(len(folds)):
-            if i == j:
-                continue
-
-            start_index = 0
-            for k in range(j):
-                start_index += len(folds[k])
-            fold_length = len(folds[j])
-            end_index = start_index + fold_length
-
-            classifier.partial_fit(X[start_index:end_index], [True] * (fold_length // 2) + \
-                                                             [False] * (fold_length // 2), \
-                                                             [True, False])
+        classifier = SVC(gamma="auto", kernel="linear") if nb_or_svm[0] == "svm" \
+                else MultinomialNB(alpha=(1.0 if nb_or_svm[1] else 1.0e-10))
 
         start_index = 0
         for j in range(i):
             start_index += len(folds[j])
         end_index = start_index + len(folds[i])
 
+        test_set = X[start_index:end_index]
+        training_set = vstack([X[:start_index], X[end_index:]])
+        classifier.fit(training_set, [f[1] for fold in (folds[:i] + folds[i+1:]) for f in fold])
+
         correct_predictions = 0
-        results = classifier.predict(X[start_index:end_index])
+        results = classifier.predict(test_set)
         for j in range(len(results)):
-            correct_predictions += int(results[j] == (j < len(folds[i]) // 2))
+            correct_predictions += int(results[j] == folds[i][j][1])
 
         accuracies.append(100 * correct_predictions/len(results))
 
-    print("smoothed" if smoothed else "unsmoothed", \
-          "stemmed" if stemming else "unstemmed", \
+    if nb_or_svm[0] != "svm":
+        print("smoothed" if nb_or_svm[1] else "unsmoothed", end=" ")
+
+    print("stemmed" if stemming else "unstemmed", \
           "presence" if binary else "frequency", \
           "unigrams" if ngrams == (1, 1) else \
           ("bigrams" if ngrams == (2, 2) else \
@@ -73,17 +57,24 @@ def perform_tests(num_folds):
 
     folds = [[] for i in range(num_folds)]
     for i in range(len(pos_files)):
-        folds[i % num_folds].append(pos_files[i])
+        folds[i % num_folds].append((pos_files[i], True))
     for i in range(len(neg_files)):
-        folds[i % num_folds].append(neg_files[i])
+        folds[i % num_folds].append((neg_files[i], False))
 
     print("\nUsing", num_folds, "fold cross-validation.\n")
+
+    print("SVM\n***")
+    for stemming in [True, False]:
+        for binary in [True, False]:
+            for ngrams in [(1, 1), (2, 2), (1, 2)]:
+                classify(folds, ("svm",), ngrams, stemming, binary)
+    print()
 
     print("Naive Bayes\n***********")
     for smoothed in [True, False]:
         for stemming in [True, False]:
             for binary in [True, False]:
                 for ngrams in [(1, 1), (2, 2), (1, 2)]:
-                    naive_bayes(folds, smoothed, ngrams, stemming, binary)
+                    classify(folds, ("nb", smoothed), ngrams, stemming, binary)
 
 perform_tests(3)
